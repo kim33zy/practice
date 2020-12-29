9975de9c66bfa5d57fd45f14313224cf0e30967a
kim33zy <kayde348@gmail.com>
2020-12-30 05:11:47 +0900

    setting label

--- MyTimer/MyTimer/ViewController.swift
+++ MyTimer/MyTimer/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainLabel.font = UIFont.systemFont(ofSize: 30.0)
+        mainLabel.textAlignment = .center
+        mainLabel.text = "REMAINED TIME"
+    
     }
 
 
