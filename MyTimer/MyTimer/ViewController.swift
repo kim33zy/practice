42466de794a98b12e92a53dc2538a1394e5377e5
kim33zy <kayde348@gmail.com>
2020-12-30 04:22:03 +0900

    first commit

---
+++ MyTimer/MyTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by Kim Kyung Dong on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
