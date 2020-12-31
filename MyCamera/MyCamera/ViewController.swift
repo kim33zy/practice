18244a4ed34ff252007458d9c451a9177a769dfc
kim33zy <kayde348@gmail.com>
2020-12-31 21:39:04 +0900

    camera

---
+++ MyCamera/MyCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by Kim Kyung Dong on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
