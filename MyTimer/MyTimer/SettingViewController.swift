7ea57bc2dbdf3247e0cc5d18083ca973cda932cf
kim33zy <kayde348@gmail.com>
2020-12-31 19:19:06 +0900

    finish timer

--- MyTimer/MyTimer/SettingViewController.swift
+++ MyTimer/MyTimer/SettingViewController.swift
@@ -7,15 +7,51 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return settingArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(settingArray[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let settings = UserDefaults.standard
+        settings.setValue(settingArray[row], forKey: settingKey)
+        settings.synchronize()
+    }
 
+    let settingArray : [Int] = [10, 20, 30, 40, 50, 60]
+    let settingKey = "timer_value"
+    
     @IBOutlet weak var timerSettingPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        timerSettingPicker.delegate = self
+        timerSettingPicker.dataSource = self
+        
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        for row in 0..<settingArray.count {
+            if settingArray[row] == timerValue {
+                timerSettingPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
+
     }
     @IBAction func decisionButtonAction(_ sender: Any) {
+    
+        _ = navigationController?.popViewController(animated: true)
+    
     }
     
 }
