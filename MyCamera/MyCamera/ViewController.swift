21d5fcae22f978bbe74cf5bf0f0403163c11196b
kim33zy <kayde348@gmail.com>
2021-01-01 00:13:13 +0900

    add share function

--- MyCamera/MyCamera/ViewController.swift
+++ MyCamera/MyCamera/ViewController.swift
@@ -29,15 +29,25 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             print("camera is not available")
         }
         
-        func imagePickerController(_ picker : UIImagePickerController, didFinishPickingMediaWithInfo info : [UIImagePickerController.InfoKey : Any]) {
-            pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-            dismiss(animated: true, completion: nil)
-        }
+
         
     }
     @IBAction func shareButtonAction(_ sender: Any) {
+        
+        if let shareImage = pictureImage.image {
+            let shareItems = [shareImage]
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller, animated: true, completion: nil)
+        }
+        
+        
     }
     
+    func imagePickerController(_ picker : UIImagePickerController, didFinishPickingMediaWithInfo info : [UIImagePickerController.InfoKey : Any]) {
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
